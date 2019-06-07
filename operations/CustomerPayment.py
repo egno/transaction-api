69af562c4c1ab398ef47eef8e8a02defe33af41f
Egno <ex.lxndr@gmail.com>
2019-06-07 16:38:00 +0700

    fix numeric

--- operations/CustomerPayment.py
+++ operations/CustomerPayment.py
@@ -6,28 +6,33 @@ def do(tr, params):
     tr.data['description'] = 'Пополнен баланс ЛС'
     tr.postTransaction()
 
+    amount = float(params['amount'])
+    payCommission = float(params.get('payCommission', 0))
+    unoCommission = float(params.get('unoCommission', 0))
+
     tr.postEntry(account='paymentSystemDebt',
-                 amount=params['amount'],
+                 amount=amount,
                  analytics={'provider': params.get('provider')},
                  data={'description': 'Сформирована задолженность ПС'})
 
     tr.postEntry(account='paymentSystemComission',
-                 amount=params['payCommission'],
+                 amount=payCommission,
                  analytics={'provider': params.get('provider')},
                  data={'description': 'Учтена комиссия платежной системы'})
 
     tr.postEntry(account='income',
-                 amount=params.get('unoCommission'),
+                 amount=unoCommission,
                  analytics={
                      'business': params['business'], 'provider': params.get('provider')},
                  data={'description': 'Получена выручка от предоставления возможности онлайн платежей'})
 
-    tr.postEntry(account='business', amount=params['amount'],
+    tr.postEntry(account='business',
+                 amount=amount,
                  analytics={'business': params['business']},
                  data={'description': 'Пополнен баланс'})
 
     tr.postEntry(account='business',
-                 amount=-params.get('unoCommission'),
+                 amount=-unoCommission,
                  analytics={'business': params['business']},
                  data={'description': 'Списана комиссия за предоставление возможности онлайн платежей'})
 
